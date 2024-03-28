9270894ca4dcd7af062b19767cbaa3b23ec84899
Ayyan Inam <ayaaninam555@gmail.com>
2024-03-28 09:09:16 +0500

    FINAL COMMIT

--- euka_email/settings.py
+++ euka_email/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-p)q07rcj4coqtn43d+b4pj^3-^&cf%8@-v#$$3qlk#w6nh4y^n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['email.euka.ai', '143.198.61.222']
 
 
 
