8d0a47f355672b8436cda329ad4b952d952aeec8
Ayyan Inam <ayaaninam555@gmail.com>
2024-03-29 07:44:50 +0500

    Removed Sensitive Pages

--- base/urls.py
+++ base/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = "base"
 
 urlpatterns = [
-    path('', views.home, name="home"),
     path('api/', include('base.api.urls')),
-    path('login', views.login, name="login"),
-    path('logout', views.logout, name="logout"),
-    path('view_email', views.view_email, name="view_email"),
-    path('add_email', views.add_email, name="add_email"),
-    path('add_campaign', views.add_campaign, name="add_campaign"),
-    path('generate-fake-email', views.generate_fake_email, name="generate_fake_email"),
+    # path('', views.home, name="home"),
+    # path('login', views.login, name="login"),
+    # path('logout', views.logout, name="logout"),
+    # path('view_email', views.view_email, name="view_email"),
+    # path('add_email', views.add_email, name="add_email"),
+    # path('add_campaign', views.add_campaign, name="add_campaign"),
+    # path('generate-fake-email', views.generate_fake_email, name="generate_fake_email"),
 ]
